016be9f6cf0e0db96be7544833d5f00ccef68dcd
TheoLeDozo <theo.broqua2002@gmail.com>
2023-10-10 14:56:50 +0200

    feat!: add a basic api to the app
    BREAKING CHANGE : ADD the Lab3 basic web API

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const app = express();
 const home = require('./routes')
 const hello = require('./routes/hello')
 const about = require('./routes/about')
+const article = require('./routes/articles')
 
+app.use(express.json());
 app.use('/', home);
 app.use('/hello', hello);
 app.use('/about', about);
+app.use('/article', article);
 
 app.set('port', 8080);
 
