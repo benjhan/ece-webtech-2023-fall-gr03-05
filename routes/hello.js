989a4dc19a50cf4d0fb85bf50b19fa487783f82c
Benryo <benryo88@hotmail.com>
2023-10-17 18:44:02 +0200

    feat: lab04 create a local website with next.js é

--- routes/hello.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/:name', (req, res) => {
-  res.writeHead(200)
-  res.send("Hello " + req.params.name)
-})
-
-module.exports = router
