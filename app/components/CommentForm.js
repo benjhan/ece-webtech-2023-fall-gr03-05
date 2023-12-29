72d21ceaad74a1b695eada81b84b80c7e03743ef
Benjamin HAN <benryo88@hotmail.com>
2023-12-29 13:11:05 +0100

    styles: changes button style

--- app/components/CommentForm.js
+++ app/components/CommentForm.js
@@ -2,39 +2,66 @@ import { useState } from 'react';
 import { useDarkMode } from '@/components/DarkModeContext';
 
 const CommentForm = ({ onSubmit }) => {
- const { darkMode } = useDarkMode();
- const [email, setEmail] = useState('');
- const [commentText, setCommentText] = useState('');
+  const { darkMode } = useDarkMode();
+  const [email, setEmail] = useState('');
+  const [commentText, setCommentText] = useState('');
+  const [hoveredButton, setHoveredButton] = useState('');
 
- const postBlockStyle = {
-   backgroundColor: darkMode ? '#333' : 'white', // Dark background for dark mode
-   color: darkMode ? 'white' : 'black', // Text color for dark mode
- };
+   const formStyle = {
+    backgroundColor: darkMode ? '#333' : 'white', // Dark background for dark mode
+    color: darkMode ? 'white' : 'black', // Text color for dark mode
+    padding: '20px',
+    borderRadius: '10px',
+  };
 
- const handleSubmit = (event) => {
-   event.preventDefault();
-   onSubmit({ email, commentText });
-   setEmail('');
-   setCommentText('');
- };
+  const inputStyle = {
+    backgroundColor: darkMode ? '#555' : 'white',
+    color: darkMode ? 'white' : 'black',
+    width: '100%',
+    padding: '10px',
+    margin: '10px 0',
+    borderRadius: '5px',
+    border: darkMode ? '1px solid #777' : '1px solid #ccc',
+    margin: '10px 0',
+  };
 
- return (
-   <div className="max-w-md mx-auto shadow-md p-6 rounded-md mb-4" style={postBlockStyle}>
-     <form onSubmit={handleSubmit}>
-       <div>
-         <label htmlFor="email">Email:</label>
-         <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required style={postBlockStyle} />
-       </div>
-       <div>
-         <label htmlFor="commentText">Comment:</label>
-         <textarea id="commentText" value={commentText} onChange={(e) => setCommentText(e.target.value)} required style={postBlockStyle} />
-       </div>
-       <div>
-         <button type="submit">Submit</button>
-       </div>
-     </form>
-   </div>
- );
+  const buttonStyle = (buttonName) => ({
+    backgroundColor: '#0070f3',
+    color: 'white',
+    padding: '10px',
+    borderRadius: '5px',
+    marginBottom: '10px',
+    border: 'none',
+    cursor: 'pointer',
+    width: '100%', 
+    transition: 'transform 0.3s ease', // Animation transition
+    transform: hoveredButton === buttonName ? 'scale(1.05)' : 'scale(1)', // Scale the button
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({ email, commentText });
+    setEmail('');
+    setCommentText('');
+  };
+
+  return (
+    <div className="max-w-md mx-auto shadow-md p-6 rounded-md mb-4" style={formStyle}>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email" style={{ display: 'block', marginBottom: '5px' }}>Email:</label>
+          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required style={inputStyle} />
+        </div>
+        <div>
+          <label htmlFor="commentText" style={{ display: 'block', marginBottom: '5px' }}>Comment:</label>
+          <textarea id="commentText" value={commentText} onChange={(e) => setCommentText(e.target.value)} required style={{ ...inputStyle, height: '100px' }} />
+        </div>
+        <div>
+          <button type ="submit" style={buttonStyle('post')} onMouseEnter={() => setHoveredButton('post')} onMouseLeave={() => setHoveredButton('')}>Edit</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CommentForm;
--- app/pages/about.js
+++ app/pages/about.js
@@ -5,7 +5,7 @@ export default function Page() {
       <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '60vh', textAlign: 'center', padding: '0 20px'}}>
         <h1 style={{ marginBottom: '20px', fontSize: '2rem', fontWeight: 'bold'}}>About Us</h1>
         <div style={{backgroundColor: '#9e9e9e',borderRadius: '10px', padding: '20px',boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',maxWidth: '600px',}}>
-          <h2>Hey, Travel Buddies! :wave:</h2>
+          <h2>Hey, Travel Buddies !</h2>
           <p>
             Welcome to our little corner of the internet where we spill the tea on all things travel. We're a bunch of ECE Paris peeps who can't sit still, always out there finding the next cool spot to chill or thrill.
           </p>
